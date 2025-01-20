da229ef66a0286244bd5067191a37dc9c8727909
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-01-20 01:34:53 -0300

    feat: implemented the static pages home, about, courses and contact

---
+++ src/main/java/com/marcusvbrangel/eazyschool/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.marcusvbrangel.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String contactPage() {
+        return "contact.html";
+    }
+}
