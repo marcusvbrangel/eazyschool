11108dfe42af7564cad3d57333590fef95766409
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-01-21 01:05:03 -0300

    refac: implemeted refactor on model classes, changed de explicits member class to lombok

--- src/main/java/com/marcusvbrangel/eazyschool/service/ContactService.java
+++ src/main/java/com/marcusvbrangel/eazyschool/service/ContactService.java
@@ -1,15 +1,12 @@
 package com.marcusvbrangel.eazyschool.service;
 
-import com.marcusvbrangel.eazyschool.controller.ContactController;
 import com.marcusvbrangel.eazyschool.model.Contact;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
-
-    private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
