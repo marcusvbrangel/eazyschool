c6641557af587864c00fe71d45ff11f43e66405e
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-01-21 23:00:24 -0300

    feat: implemented session scope on contact service

--- src/main/java/com/marcusvbrangel/eazyschool/service/ContactService.java
+++ src/main/java/com/marcusvbrangel/eazyschool/service/ContactService.java
@@ -3,14 +3,30 @@ package com.marcusvbrangel.eazyschool.service;
 import com.marcusvbrangel.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
+@SessionScope
 public class ContactService {
+
+    private int counter = 0;
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
         log.info(contact.toString());
         return isSaved;
+    }
+
+    public ContactService() {
+        System.out.println("Contact Service Bean Initialized");
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
