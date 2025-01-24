e2ab1b24524ad03de02561a5c80ab4f6ddb9aaa7
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-01-24 01:24:36 -0300

    feat: implemented csrf security

--- src/main/java/com/marcusvbrangel/eazyschool/config/security/SecurityConfig.java
+++ src/main/java/com/marcusvbrangel/eazyschool/config/security/SecurityConfig.java
@@ -25,8 +25,9 @@ public class SecurityConfig {
 //            .formLogin(Customizer.withDefaults())
 //            .httpBasic(Customizer.withDefaults());
 
-        http.csrf((csrf) -> csrf.disable())
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
             .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/test/**").permitAll()
                 .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/", "home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
@@ -35,6 +36,7 @@ public class SecurityConfig {
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
                 .requestMatchers("/login").permitAll()
+                .requestMatchers("/logout").permitAll()
             .requestMatchers("/assets/**").permitAll())
             .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
                 .defaultSuccessUrl("/dashboard")
